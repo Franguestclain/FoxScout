41a92acd2dc4f322f6e422b75905ff860b1ad203
RicRam023 <raramirezgutierrez@gmail.com>
2019-07-24 21:15:33 -0600

    se creo el archivo sucursales con altas, bajas y update

--- client/admin/sucursales.php
+++ client/admin/sucursales.php
@@ -50,21 +50,147 @@
                         </button>
                     </div>
                     <div id="modal-body-sucursal" class="modal-body">
-                        <form id="addTienda" action="./actions/regTienda.php" method="POST" enctype="multipart/form-data"> <!-- si nuestro form utiliza un input file, necesitamos incluid enctype="multipart/form-data" -->
+                        <form id="addSucursal" action="./actions/regSucursales.php" method="POST" > <!-- si nuestro form utiliza un input file, necesitamos incluid enctype="multipart/form-data" -->
                             <div class="form-group">
-                                <label for="addNombre">Nombre</label>
-                                <input type="text" name="addNombre" id="addNombre" class="form-control">
+                                <label for="addCalle">Calle</label>
+                                <input type="text" name="addCalle" id="addCalle" class="form-control">
                             </div>
                             <div class="form-group">
+                                <label for="addColonia">Colonia</label>
+                                <input type="text" name="addColonia" id="addColonia" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="addNumero">Numero</label>
+                                <input type="text" name="addNumero" id="addNumero" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="selectTienda">Tienda</label>
+                                <select class="form-control" name="selectTienda" id="selectTienda">
+                                    <?php
+                                        $optionTienda = "SELECT * FROM tienda";
+                                        if($resOptionE = $con -> query($optionTienda)){
+                                            if( $resOptionE -> num_rows > 0 ){
+                                                while($filaOptionE = $resOptionE -> fetch_assoc()){
+                                                    echo "<option id='option-addTienda-id{$filaOptionE['id_tienda']}' value='{$filaOptionE['id_tienda']}'>{$filaOptionE['nombre']}</option>";
+                                                }
+                                            }
+                                        }
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="addCiudad">Ciudad</label>
+                                <select class="form-control" name="selectCiudad" id="selectCiudad">
+                                    <?php
+                                        $optionCiudad = "SELECT * FROM ciudad";
+                                        if($resOptionE = $con -> query($optionCiudad)){
+                                            if( $resOptionE -> num_rows > 0 ){
+                                                while($filaOptionE = $resOptionE -> fetch_assoc()){
+                                                    echo "<option id='option-addCiudad-id{$filaOptionE['id_ciudad']}' value='{$filaOptionE['id_ciudad']}'>{$filaOptionE['nombre']}</option>";
+                                                }
+                                            }
+                                        }
+                                    ?>
+                                </select>
+                            </div>
+                            <!-- <div class="form-group">
                                 <label for="addImage">Imagen</label>
                                 <input type="file" accept="image/png, image/jpeg, image/jpg" name="addImage" id="addImage" class="form-control">
-                            </div>
+                            </div> -->
                         </form>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                        <input form="addTienda" id="registrarTienda" name="registrarTienda" type="submit" class="btn btn-primary" value="Registrar">
+                        <input form="addSucursal" id="registrarSucursal" name="registrarSucursal" type="submit" class="btn btn-primary" value="Registrar">
                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="modalEditarSucursal" tabindex="-1" role="dialog" aria-labelledby="modalAgregarSucursalTitulo" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modalAgregarSucursalTitulo">Agregar Sucursal</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div id="modal-body-sucursal" class="modal-body">
+                        <form id="editSucursal" action="./actions/editSucursales.php" method="POST" > <!-- si nuestro form utiliza un input file, necesitamos incluid enctype="multipart/form-data" -->
+                            <div class="form-group">
+                                <label for="editCalle">Calle</label>
+                                <input type="text" name="editCalle" id="editCalle" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="editColonia">Colonia</label>
+                                <input type="text" name="editColonia" id="editColonia" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="editNumero">Numero</label>
+                                <input type="text" name="editNumero" id="editNumero" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="editselectTienda">Tienda</label>
+                                <select class="form-control" name="editselectTienda" id="editselectTienda">
+                                    <?php
+                                        $optionTienda = "SELECT * FROM tienda";
+                                        if($resOptionE = $con -> query($optionTienda)){
+                                            if( $resOptionE -> num_rows > 0 ){
+                                                while($filaOptionE = $resOptionE -> fetch_assoc()){
+                                                    echo "<option id='option-editTienda-id{$filaOptionE['id_tienda']}' value='{$filaOptionE['id_tienda']}'>{$filaOptionE['nombre']}</option>";
+                                                }
+                                            }
+                                        }
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="editselectCiudad">Ciudad</label>
+                                <select class="form-control" name="editselectCiudad" id="editselectCiudad">
+                                    <?php
+                                        $optionCiudad = "SELECT * FROM ciudad";
+                                        if($resOptionE = $con -> query($optionCiudad)){
+                                            if( $resOptionE -> num_rows > 0 ){
+                                                while($filaOptionE = $resOptionE -> fetch_assoc()){
+                                                    echo "<option id='option-editCiudad-id{$filaOptionE['id_ciudad']}' value='{$filaOptionE['id_ciudad']}'>{$filaOptionE['nombre']}</option>";
+                                                }
+                                            }
+                                        }
+                                    ?>
+                                </select>
+                            </div>
+                            <input type="hidden" name="id" id="id-edit-Sucursal" value="">
+
+                            <!-- <div class="form-group">
+                                <label for="addImage">Imagen</label>
+                                <input type="file" accept="image/png, image/jpeg, image/jpg" name="addImage" id="addImage" class="form-control">
+                            </div> -->
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                        <input form="editSucursal" id="editSucursal" name="editSucursal" type="submit" class="btn btn-primary" value="Editar">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal fade" id="modalDelSucursal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">¿Estas seguro de eliminar estos registros?</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Estos datos se eliminaran de forma permanente.</p>
+                </div>
+                <div class="modal-footer">
+                    <button id="confirmarSucursal" type="button" class="btn btn-primary">Ok</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                </div>
                 </div>
             </div>
         </div>
@@ -87,9 +213,9 @@
                                         <h4 class="card-title">Sucursales</h4>
                                     </div>
                                     <div class="ml-auto">
-                                        <button class="btn btn-success btnAgregar" data-toggle="modal" data-target=""><i class="fa fa-plus"></i> Agregar</button>
-                                        <button class="btn btn-primary btnEditar"><i class="fa fa-sync-alt"></i> Editar</button>
-                                        <button class="btn btn-danger btnEliminar"><i class="fa fa-trash"></i> Eliminar</button>
+                                        <button class="btn btn-success btnAgregar" data-toggle="modal" data-target="#modalAgregarSucursal"><i class="fa fa-plus"></i> Agregar</button>
+                                        <button id="btnEditarSucursal" class="btn btn-primary btnEditar"><i class="fa fa-sync-alt"></i> Editar</button>
+                                        <button id="btnEliminarSucursal" class="btn btn-danger btnEliminar"><i class="fa fa-trash"></i> Eliminar</button>
                                     </div>
                                 </div>
                             </div>
@@ -98,8 +224,8 @@
                                     <thead>
                                         <tr class="bg-light">
                                             <th class="border-top-0">
-                                                <input style="display: none;" class="inp-cbx" type="checkbox" name="checkAll-Direccion" id="checkAll-Direccion">
-                                                <label class="cbx" for="checkAll-Direccion">
+                                                <input style="display: none;" class="inp-cbx" type="checkbox" name="checkAll-Sucursal" id="checkAll-Sucursal">
+                                                <label class="cbx" for="checkAll-Sucursal">
                                                     <span>
                                                         <svg width="12px" height="10px" viewbox="0 0 12 10">
                                                             <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
@@ -117,15 +243,15 @@
                                     </thead>
                                     <tbody>
                                         <?php
-                                            $listar = "SELECT * FROM direccion";
+                                            $listar = "SELECT d.*, t.nombre nombreT, c.nombre nombreC FROM direccion d, tienda t, ciudad c WHERE tienda_id=id_tienda && ciudad_id=id_ciudad";
 
                                             if( $res = $con -> query($listar) ){
                                                 if( $res -> num_rows > 0 ){
                                                     while($fila = $res -> fetch_assoc() ){
-                                                        echo "<tr id='row-{$fila['id_direccion']}'>";
+                                                        echo "<tr id='row-Sucursal{$fila['id_direccion']}'>";
                                                             echo "<td>";
-                                                                echo "<input style='display: none;' class='inp-cbx checkboxDireccion' type='checkbox' data-idRow='{$fila['id_direccion']}' name='check-Direccion{$fila['id_direccion']}' id='check-Direccion{$fila['id_direccion']}'>";
-                                                                echo "<label class='cbx' for='check-Direccion{$fila['id_direccion']}'>";
+                                                                echo "<input style='display: none;' class='inp-cbx checkboxSucursal' type='checkbox' data-idRow='{$fila['id_direccion']}' name='check-Sucursal{$fila['id_direccion']}' id='check-Sucursal{$fila['id_direccion']}'>";
+                                                                echo "<label class='cbx' for='check-Sucursal{$fila['id_direccion']}'>";
                                                                     echo "<span>";
                                                                         echo "<svg width='12px' height='10px' viewbox='0 0 12 10'>";
                                                                             echo "<polyline points='1.5 6 4.5 9 10.5 1'></polyline>";
@@ -167,6 +293,7 @@
         <!-- ============================================================== -->
         <!-- End Page wrapper  -->
         <!-- ============================================================== -->
+        <div id="alertGenSucursal" class="alert bg-dark alertGen fadeOutDown"></div>
     </div>
     <!-- ============================================================== -->
     <!-- End Wrapper -->
